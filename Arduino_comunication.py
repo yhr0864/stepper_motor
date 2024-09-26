396b2cfc2cfe750214d37c040322df62581b076c
yhr0864 <90278898+yhr0864@users.noreply.github.com>
2024-09-26 15:25:16 +0200

    Update Arduino_comunication.py

--- Arduino_comunication.py
+++ Arduino_comunication.py
@@ -1,17 +1,20 @@
 import serial
-import time
 
 arduino = serial.Serial(port="COM8", baudrate=9600, timeout=0.1)
 
 
 def write_read(x):
-    arduino.write(bytes(x, "utf-8"))
-    time.sleep(0.005)
-    data = arduino.readline()
-    return data
+    # Send data to Arduino
+    arduino.write(bytes(x, "utf-8"))  # Send with newline
+
+    # Wait for a response
+    while True:
+        data = arduino.readline().decode("utf-8").strip()
+        if data:  # Check if data is received
+            return data
 
 
 while True:
     num = input("Enter a number: ")  # Taking input from user
-    value = write_read(num)
-    print(value)  # printing the value
+    value = write_read(num)  # Send input and get the response
+    print("Received:", value)  # Print the response
